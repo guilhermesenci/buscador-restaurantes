e96540b340db149821ba29a81f46371538c2c453
Guilherme Senci <guilherme.g.senci@gmail.com>
2021-08-11 16:40:04 -0300

    adicionando estilos

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { Reset } from 'styled-reset'
+
+import './App.css';
+import Home from './pages/Home/index'
+import theme from './theme'
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Reset />
+      <Home />
+    </ThemeProvider>
+  );
+}
+
+export default App;
